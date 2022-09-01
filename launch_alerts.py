0fd30492d19bd45efc6075d43741f6e16d72b0b9
Jorge <me@juniorjorge.com>
2022-09-01 16:06:09 -0400

    Move the message send to after the long async for loop

--- launch_alerts.py
+++ launch_alerts.py
@@ -157,8 +157,6 @@ async def send_launch_alert(lm: LaunchMonitor) -> None:
     if isinstance(channel, GuildChannel) and channel.guild.id == 360523650912223253 and launch["vehicle"]["id"] == 115:
         channel = bot.get_channel(int(754432168293433354))
 
-    asyncio.ensure_future(send_launch_panel(channel, launch, config.timezone, message="There's a launch coming up!"))
-
     async for msg in channel.history(limit = 3):
         if msg.embeds and msg.author == bot.user:
             if msg.embeds[0].title == launch["name"]:
@@ -167,6 +165,7 @@ async def send_launch_alert(lm: LaunchMonitor) -> None:
                     #Skip alerting everyone
                     return
 
+    asyncio.ensure_future(send_launch_panel(channel, launch, config.timezone, message="There's a launch coming up!"))
 
 @backoff.on_exception(backoff.expo,
                       Exception,
